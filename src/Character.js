9992ae5de77bc7a269501d3ee145d6074fe7e07b
pdizz <pete.albrecht@gmail.com>
2015-08-24 09:56:50 -0600

    improved ability mod calculation

--- src/Character.js
+++ src/Character.js
@@ -5,13 +5,7 @@ function Character() {
 
     // 1(-5), 2(-4), 3(-4), 4(-3), 5(-3), 6(-2), 7(-2) http://www.d20srd.org/srd/theBasics.htm#abilityScores
     var getModifier = function (ability) {
-        var mod = -5;
-        for (var i = 0; i < ability; i++) {
-            if (i % 2 === 1) {
-                mod++
-            }
-        }
-        return mod;
+        return Math.floor(ability / 2) - 5;
     }
 }
 
