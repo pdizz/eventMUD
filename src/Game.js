7ab436acbf5a3c7e87c9db5c5d5d7492c690c780
pdizz <pete.albrecht@gmail.com>
2015-08-24 22:47:13 -0600

    player room and "say" command

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 var Player = require(__dirname + '/Player.js');
+var Character = require(__dirname + '/Character.js');
 var CharacterCreation = require(__dirname + '/Dialog/CharacterCreation.js');
 
 function Game(io, http, dice) {
@@ -16,8 +17,24 @@ function Game(io, http, dice) {
         var player = new Player(socket);
         me.players.push(player);
 
-        var charCreation = new CharacterCreation(player, dice);
-        charCreation.create();
+
+        // TODO more rooms
+        player.room = 'default';
+        socket.join('default');
+
+        // Commands
+        socket.on('message', function (command) {
+            var words = command.split(' ');
+            switch (words[0].toLowerCase()) {
+                case 'say':
+                    var message = words.slice(1).join(' ');
+                    socket.to(player.room).emit('message', player.socket.id + ' says "' + message + '"');
+                    socket.emit('message', 'You say "' + message + '"');
+                    break;
+                default:
+                    socket.emit('message', 'Invalid command!');
+            }
+        });
 
         socket.on('disconnect', function(){
             me.players.splice(me.players.indexOf(player));
--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,6 @@
 function Player(socket) {
-    var me = this;
-
     this.socket    = socket;
     this.character = {};
-
-    this.socket.on('message', function(msg){
-        console.log(msg);
-    });
 }
 
 module.exports = Player;
